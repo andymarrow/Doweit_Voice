11ecaf4b9d108aaa6828bd72343cfd82c93900af
patrix726 <a0911991679@gmail.com>
2025-09-02 11:42:15 +0300

    refactor(configs): changed file formats removed unnecessary files and configs

---
+++ configs/constants.js
@@ -0,0 +1,3 @@
+export const GEMINI_API_KEY = process.env.GEMNI_API_KEY; // Ensure GEMINI_API_KEY is in your environment variables
+export const FIREBASE_API_KEY = process.env.NExt_PUBLIC_FIREBASE_API_KEY;
+export const VAPI_PUBLIC_KEY = process.env.NEXT_PUBLIC_VAPI_PUBLIC_KEY;
