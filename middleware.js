52201c032ad666c154db0f1c60cad53ae131a801
andymarrow <miheretabsamson90@gmail.com>
2025-11-28 11:10:27 +0300

    middleware cookies update

--- middleware.js
+++ middleware.js
@@ -24,9 +24,9 @@ export async function middleware(request) {
 
 export const config = {
 	matcher: [
-		// Skip Next.js internals and all static files, unless found in search paramss
+		// Skip Next.js internals and all static files, unless found in search paramsss
 		"/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-		// Always run for API routes
+		// Always run for API routess
 		// '/(api|trpc)(.*)',
 	],
 };
